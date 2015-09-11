030fa478b2c5de7cb96bc77b9b84304e87b5bae0
Anna Miroshnik <anna.miroshnik@jamaza.com>
2015-09-11 13:17:18 +0300

    init commit

---
+++ src/test/TestServlet.java
@@ -0,0 +1,48 @@
+/*****************************************************************/
+/* Copyright 2013 Code Strategies                                */
+/* This code may be freely used and distributed in any project.  */
+/* However, please do not remove this credit if you publish this */
+/* code in paper or electronic form, such as on a web site.      */
+/*****************************************************************/
+
+package test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.codec.binary.Base64;
+
+
+public class TestServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("This is the Test Servlet");
+
+		Enumeration headerNames = request.getHeaderNames();
+		while (headerNames.hasMoreElements()) {
+			String headerName = (String) headerNames.nextElement();
+			out.print("<br/>Header Name: <em>" + headerName);
+			String headerValue = request.getHeader(headerName);
+			out.print("</em>, Header Value: <em>" + headerValue);
+			out.println("</em>");
+		}
+
+		out.println("<hr/>");
+		String authHeader = request.getHeader("authorization");
+		String encodedValue = authHeader.split(" ")[1];
+		out.println("Base64-encoded Authorization Value: <em>" + encodedValue);
+		String decodedValue = Base64.decodeBase64(encodedValue).toString();
+		out.println("</em><br/>Base64-decoded Authorization Value: <em>" + decodedValue);
+		out.println("</em>");
+	}
+
+}
